d0b0e06ec2dd1f11cf5931a977a5732c3e0ac883
Daniel Gorin <jcpetruzza@gmail.com>
2018-06-09 14:35:15 +0000

    shell.nix that with our packages

--- nix/shell.nix
+++ nix/shell.nix
@@ -1,10 +1,13 @@
 {nixpkgs ? import ./nixpkgs.nix {}}:
 
-nixpkgs.haskell.lib.buildStackProject {
-  name = "test-nix-thing";
+let
+  packagesToInstall = nixpkgs.pkgs.haskell.packages.stackage.lib.callStackage2nix "zurihac18" ../. {inherit nixpkgs;};
+in
+  nixpkgs.haskell.lib.buildStackProject {
+    name = "test-nix-thing";
 
-  buildInputs = with nixpkgs.pkgs; [
-    git   # used by stack when downloading github repos, etc
-    ((haskell.packages.stackage.lib.callStackage2nix "zurihac18" ../.).ghcWithPackages (ps: [ps.text]))
-  ];
-}
+    buildInputs = with nixpkgs.pkgs; [
+      git   # used by stack when downloading github repos, etc
+      (packagesToInstall.ghc.withPackages (ps: [ps.foo ps.bar]))
+    ];
+  }
