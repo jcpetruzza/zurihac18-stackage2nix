8e18179dd872251c3e8e9ba4e5b37e7072335cf7
Daniel Gorin <jcpetruzza@gmail.com>
2018-06-10 09:05:49 +0000

    Version of shell.nix that works less badly

--- nix/shell.nix
+++ nix/shell.nix
@@ -1,13 +1,27 @@
 {nixpkgs ? import ./nixpkgs.nix {}}:
 
 let
-  packagesToInstall = nixpkgs.pkgs.haskell.packages.stackage.lib.callStackage2nix "zurihac18" ../. {inherit nixpkgs;};
+  stackWorkFilter = name: type: let baseName = baseNameOf (toString name); in ! (
+    (type == "directory" && baseName == ".stack-work")
+  );
+
+  srcDir = builtins.filterSource stackWorkFilter ../.;
+
+  packagesToInstall = nixpkgs.pkgs.haskell.packages.stackage.lib.callStackage2nix "zurihac18" srcDir {inherit nixpkgs;};
+
+  makeOverridable = nixpkgs.lib.makeOverridable;
+
+  ghc_with_packages = packagesToInstall.ghc.withPackages (ps: [ps.foo ps.bar]);
+
+  buildStackProject = args: (nixpkgs.haskell.lib.buildStackProject args).overrideAttrs (oldAttrs: {
+    STACK_IN_NIX_EXTRA_ARGS = "";
+  });
 in
-  nixpkgs.haskell.lib.buildStackProject {
+  buildStackProject {
     name = "test-nix-thing";
 
-    buildInputs = with nixpkgs.pkgs; [
+      buildInputs = with nixpkgs.pkgs; [
       git   # used by stack when downloading github repos, etc
-      (packagesToInstall.ghc.withPackages (ps: [ps.foo ps.bar]))
+      ghc_with_packages
     ];
   }
